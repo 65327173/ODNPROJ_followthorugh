5acc6f6f917aac4a3c6a078f6a02a5f8daef6f2d
zach avenarius <zavenarius00@gmail.com>
2024-02-09 00:49:30 -0500

    message

---
+++ FOUNDATIONS/Asngmntfunctions.js
@@ -0,0 +1,22 @@
+function add7(num) {
+
+    return num + 7;
+}
+
+function multiply(num, num2) {
+
+    
+    return num * num2;
+}
+
+function capitalize(string) {
+
+    let uc = string.substr(0, 0).toUpperCase;
+    let lc = string.substr(1).toLowerCase;
+    return uc.concat(lc); 
+}
+
+function lastLetter(string) {
+
+return string.charAt(string.length - 1);
+}
--- FOUNDATIONS/js-notes.js
+++ FOUNDATIONS/js-notes.js
@@ -9,3 +9,68 @@ massage = "hello";
 //const can be used to define variables that are unchanging, common practice to uppercase const variables//
 const MESSIEGE = 123;
 
+// declaring strings
+const string = "hello"
+console.log(string);
+
+//Property access (returns undenfined, while charAt() returns empty string)
+let text = "HELLO WORLD!";
+
+text[0] = 'A';
+
+
+// embedding js
+
+const name  = 'zach';
+const greeting = 'Hello, ${name}';
+console.log(greeting);
+
+//concatenation
+
+function greet() {
+
+    const name = prompt("what is your name?");
+    const greeting = document.querySelector('#greeting');
+    greeting.textcontent = 'Hello ${name}, nice to meet you!';
+}
+button.addEventListener("click", greet);
+
+//slice()
+//substring(same as slice except value lower than 0 are treated as 0)
+//substr(second parameter specifies length)
+let text = "hello world";
+let text2 = text.slice(5, 10);
+
+//uppercase()/lowercase()
+let text3 = text.toLowerCase;
+let text4 = text.toUpperCase;
+
+//concat()
+let text5 = text3.concat(" ", text2);
+
+//trim(trimStart() removes whitespace from start of string, trimEnd() removes for end)
+let text6 = "             Hello World!                      ";
+let text7 = text6.trim();
+
+//padding(padStart() adds from start of string, padEnd() adds for end, for some reason whitespace doesnt work ಠಿ_ಠ)
+let paddedtext = text7.padStart(4, "0");
+let paddedtext2 = paddedtext.padStart(4, "0");
+
+//repeat(creates string of repeated string)
+let text8 = text.repeat(5);
+
+// arrow function shorthand functions
+let sum = (a, b) => a + b;
+
+/* This arrow function is a shorter form of:
+
+let sum = function(a, b) {
+  return a + b;
+};
+*/
+
+
+
+
+
+
