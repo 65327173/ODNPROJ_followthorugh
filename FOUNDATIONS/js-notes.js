1a88a43edd54dd931f9da9f644c019040617c4e0
zach avenarius <zavenarius00@gmail.com>
2024-02-09 00:46:28 -0500

    message

---
+++ FOUNDATIONS/js-notes.js
@@ -0,0 +1,11 @@
+//define variables with let method //
+// repeated declaration causes error //
+let message;
+message = "hello";
+// older vers of declaring variables //
+var massage;
+massage = "hello";
+
+//const can be used to define variables that are unchanging, common practice to uppercase const variables//
+const MESSIEGE = 123;
+
